31ae8c34eab95444804cd438b3109db9527242f2
kaku0715 <kateryna.kuntseva@NetCracker.com>
2025-03-06 12:46:25 +0200

    add products throught products.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,34 +1,6 @@
 console.log('Hello amazon');
 
-const products = [
-  {
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    priceCents: 1090,
-    rating: {
-      stars: 4.5,
-      count: 87
-    }
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    priceCents: 2095,
-    rating: {
-      stars: 4,
-      count: 127
-    }
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: ' Adults Plain Cotton T-Shirt - 2 Pack',
-    priceCents: 799,
-    rating: {
-      stars: 4.5,
-      count: 56
-    }
-  }
-];
+
 
 let productsHTML = '';
 products.forEach(
@@ -53,7 +25,7 @@ products.forEach(
           </div>
 
           <div class="product-price">
-          $${product.priceCents/100}
+          $${(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
